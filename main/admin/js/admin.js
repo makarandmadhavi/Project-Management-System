128964eb258c9386190cdec0e5ddf7f4cd6b24f6
Makarand <makarandmadhavi99@gmail.com>
2020-07-12 17:00:48 +0530

    add faculty

--- main/admin/js/admin.js
+++ main/admin/js/admin.js
@@ -1,11 +1,43 @@
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
     document.body.style.backgroundColor = "white";
+}
+
+function add_faculty(f) {
+    $('#submit').prop("disabled",true);
+    name = f.name.value;
+    email = f.email.value;
+    phone = f.phone.value;
+    domain = JSON.stringify($(f.domain).val());
+    department = f.department.value;
+    //console.log(domain);
+    $.ajax({
+        type: "POST",
+        url: "backend/addfaculty_ajax.php",
+        data: {
+            //data goes here
+            name,
+            email,
+            phone,
+            domain,
+            department
+        },
+        success: function (data) {
+            //data is returned here
+            $('#submit').prop("disabled",false);
+            if (data == "SUCCESS") {
+                alert("faculty added");
+                window.location = '';
+            } else {
+                alert("Error");
+                window.location = '';
+            }
+        }
+    });
 }
