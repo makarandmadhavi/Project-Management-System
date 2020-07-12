e125e80d146425aa42f01f3d0b4dd856c6ada7da
piyush gopal jha <47223560+piygot5@users.noreply.github.com>
2020-07-12 22:58:35 +0530

    Add files via upload

--- main/backend/login_handler.php
+++ main/backend/login_handler.php
@@ -6,16 +6,18 @@ include './config.php';
     $user_type = $_POST['user_type'];
 	
 	$password = ($_POST['group_password']); 
-	echo $user,$user_type;
+	
 	$check_database_query = $conn->query("SELECT * FROM login WHERE username='$user' AND password='$password' AND type='$user_type' ");
 	$check_login_query = $check_database_query->num_rows;
 
 	if($check_login_query == 1) {
 		
-
+		echo 1;
 		$_SESSION["user"] = $user;
 		
 	}
+	
+	
 		
 
 
