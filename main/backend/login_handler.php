a7e7af2e323e4067674e6291441d4d6a38f4aa91
piyush gopal jha <47223560+piygot5@users.noreply.github.com>
2020-07-12 21:37:07 +0530

    add backend folder

---
+++ main/backend/login_handler.php
@@ -0,0 +1,25 @@
+<?php  
+include './config.php';
+
+	session_start();
+    $user = $_POST['user_id'];
+    $user_type = $_POST['user_type'];
+	
+	$password = ($_POST['group_password']); 
+	echo $user,$user_type;
+	$check_database_query = $conn->query("SELECT * FROM login WHERE username='$user' AND password='$password' AND type='$user_type' ");
+	$check_login_query = $check_database_query->num_rows;
+
+	if($check_login_query == 1) {
+		
+
+		$_SESSION["user"] = $user;
+		
+	}
+		
+
+
+
+
+
+?>
