d456932544ac7dbd3afaaa83dd74201e7b50063d
piyush gopal jha <47223560+piygot5@users.noreply.github.com>
2020-07-12 22:58:00 +0530

    added validation

--- main/js/login.js
+++ main/js/login.js
@@ -3,7 +3,8 @@ $(document).ready(function(){
   var user_id = $("#user_id").val();
   var group_password = $("#group_password").val();
   var user_type = $("#user_type").val();
-  console.log(user_type);
+  
+ 
   // Returns successful data submission message when the entered information is stored in database.
   var dataString = 'user_id='+ user_id + '&group_password='+ group_password + '&user_type='+ user_type;
   // AJAX Code To Submit Form.
@@ -11,10 +12,18 @@ $(document).ready(function(){
   type: "POST",
   url:"backend/login_handler.php",
   data: dataString,
-  cache: false,
-  success: function(result){
-    alert(result);
-    window.location.href = './studentdashboard.php';
+  
+  success: function(response){
+    console.log(typeof(response),response );
+    if (response==1) {
+      //console.log("hello");
+      window.location.href = './studentdashboard.php';
+    }
+    else {
+      
+      alert('Invalid Credentials');
+    }
+    
   }
   });
   });
