461bc414dccaba8ff492d1826bae394806299d5b
Nikhil Ingale <nikhil.ingale99@gmail.com>
2020-07-14 17:06:21 +0530

    update

--- main/student/index.php
+++ main/student/index.php
@@ -19,53 +19,21 @@
     <!--Head and Navbar-->
     <div>
         <img src="../assets/images/Rait_new_logo_png.png" class="top_logo">
-        <div class="chip">
-            <img src="../assets/images/img_avatar.png" alt="Person" width="96" height="96">
-            Nikhil Ingale
-        </div>
+        <button class="login_btn" onclick="document.location='../index.php'">Login</button>
     </div>
-    <nav class="navbar navbar-expand-lg primary_color navbar-dark">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Dropdown
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="#">Action</a>
-                        <a class="dropdown-item" href="#">Another action</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="#">Something else here</a>
-                    </div>
-                </li>
-            </ul>
-            <form class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success my-2 my-sm-0 search_btn" type="submit">Search</button>
-            </form>
-        </div>
+    <nav class="navbar navbar-expand-lg primary_color navbar-dark nav_heading">
+        <h4>Welcome to Project Management System.</h4>
     </nav>
     <!--Head and Navbar Ends-->
 
     <!--Filter start-->
     
-    <div class="filter_box">
+    <div class="container filter_box">
         <div class="list_filters">
-            <p class="instruction">To form the group select your Branch : </p>
             <form action="">
+                <center><p class="instruction">To form the group select your Branch : </p></center>
+                <center>
                 <select name="Branch" class="custom-select mb-3 filter" id="branch">
                     <option selected>Select Branch</option>
                     <option value="Computer">Computer Science</option>
@@ -75,29 +43,31 @@
                     <option value="Instrumentation">Instrumentation</option>
                 </select>
                 <button type="button" class="btn btn-secondary view_list" onclick="getStudentList()">View List</button>
+                </center>
             </form>
         </div>
     </div>
 
     <!--Table-->
-    <div class="container">
+    <div class="container table_box">
         <div id="search_branch">
             <div class="row">
                 <h5>Search in branch <span id="display_branch"></span> : &nbsp;&nbsp;</h5>
                 <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" id="myInput">
             </div>
         </div>
-    </div>
-    <div class="container table-responsive" id="student_table">
+    
+        <div class="container table-responsive" id="student_table">
 
+        </div>
     </div>
     <!--Table Ends Here-->
 
 
     <!--Footer-->
-    <div class="jumbotron text-center" style="margin-bottom:0">
+    <!-- <div class="jumbotron text-center" style="margin-bottom:0">
         <p class="footer-data">Footer</p>
-    </div>
+    </div> -->
     <!--Footer Ends-->
 
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
