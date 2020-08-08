80dca0f35f319f3c69b8161853cada5ed00c88b2
piygot5 <piyushjha65@gmail.com>
2020-08-08 14:58:44 +0530

    edit view details page

--- main/admin/backend/view_details.php
+++ main/admin/backend/view_details.php
@@ -1,3 +1,5 @@
+
+
 <?php
     include 'conn.php';
     
@@ -30,112 +32,164 @@
             $paper3 = '../../student/uploads/'.$row['groupname']."/".$row['research_paper_3'];
     
 ?>
-    <div class="card-header">
-        <h5>Group Details</h5>
-    </div>
-    <div class="container grp_details">
-        <div class="row">
-            <div class="col-3">
-                <h6 id="row_data1">Group ID : </h6>
-            </div>
-            <div class="col-9">
-                <h6 id="row_data1"><span id="data_span"><?=$row['groupname']?></span></h6>
-            </div>
+
+
+
+
+<div class="accordion" id="accordionExample">
+    <div class="card">
+    <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+        <div class="card-header" id="headingOne">
+            <center>
+                <h5>Group Details :-</h5>
+            </center>
         </div>
+    </button>
+
+        <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+            <div class="card-body">
+                <div class="container grp_details">
+                    <div class="row">
+                        <div class="col-3">
+                            <h6 id="row_data1">Group ID : </h6>
+                        </div>
+                        <div class="col-9">
+                            <h6 id="row_data1"><span id="data_span"><?=$row['groupname']?></span></h6>
+                        </div>
+                    </div>
         <br>
-        <div class="row">
-            <div class="col-3">
-                <h6 id="row_data1">Member 1 : </h6>
-            </div>
-            <div class="col-9">
-                <h6 id="row_data1"><span id="data_span"><?=$row2['member1']?></span></h6>
-            </div>
-        </div>
+        <hr> 
+        <br>   
+                    <div class="row">
+                        <div class="col-3">
+                            <h6 id="row_data1">Member 1 : </h6>
+                        </div>
+                        <div class="col-9">
+                            <h6 id="row_data1"><span id="data_span"><?=$row2['member1']?></span></h6>
+                        </div>
+                    </div>
         <br>
-        <div class="row">
-            <div class="col-3">
-                <h6 id="row_data1">Member 2 : </h6>
-            </div>
-            <div class="col-9">
-                <h6 id="row_data1"><span id="data_span"><?=$row2['member2']?></span></h6>
-            </div>
-        </div>
+        <hr>
         <br>
-        <div class="row">
-            <div class="col-3">
-                <h6 id="row_data1">Member 3 : </h6>
-            </div>
-            <div class="col-9">
-                <h6 id="row_data1"><span id="data_span"><?=$row2['member3']?></span></h6>
-            </div>
-        </div>
-    </div>
-    <div class="card-header">
-        <h5>Project Details</h5>
-    </div> 
-    <div class="container grp_details">
-        <div class="row">
-            <div class="col-3">
-                <h6 id="row_data1">Project Name : </h6>
-            </div>
-            <div class="col-9">
-                <h6 id="row_data1"><span id="data_span"><?=$row['project_name']?></span></h6>
-            </div>
-        </div>
+                    <div class="row">
+                        <div class="col-3">
+                            <h6 id="row_data1">Member 2 : </h6>
+                        </div>
+                        <div class="col-9">
+                            <h6 id="row_data1"><span id="data_span"><?=$row2['member2']?></span></h6>
+                        </div>
+                    </div>
         <br>
-        <div class="row">
-            <div class="col-3">
-                <h6 id="row_data1">Domain : </h6>
-            </div>
-            <div class="col-9">
-                <h6 id="row_data1"><span id="data_span"><?=$row['domain']?></span></h6>
-            </div>
-        </div>
+        <hr>
         <br>
-        <div class="row">
-            <div class="col-3">
-                <h6 id="row_data1">Abstract : </h6>
-            </div>
-            <div class="col-9">
-                <h6 id="row_data1"><span id="data_span"><?=$row['abstract']?></span></h6>
-            </div>
-        </div>
-        <br>
-        <div class="row">
-            <div class="col-3">
-                <h6 id="row_data1">Project Presentation : </h6>
-            </div>
-            <div class="col-9">
-                <a class="btn btn-primary" href="backend/view_pdf.php?address=<?=$add?>" target="_blank">View Presentation</a>
-            </div>
-        </div>
-        <br>
-        <div class="row">
-            <div class="col-3">
-                <h6 id="row_data1">Research Papers Referred : </h6>
-            </div>
-            <div class="col-3">
-                <a class="btn btn-primary" href="backend/view_pdf.php?address=<?=$paper1?>" target="_blank">View Paper 1</a>
-            </div>
-            <div class="col-3">
-                <a class="btn btn-primary" href="backend/view_pdf.php?address=<?=$paper2?>" target="_blank">View Paper 2</a>
-            </div>
-            <div class="col-3">
-                <a class="btn btn-primary" href="backend/view_pdf.php?address=<?=$paper3?>" target="_blank">View Paper 3</a>
+                    <div class="row">
+                        <div class="col-3">
+                            <h6 id="row_data1">Member 3 : </h6>
+                        </div>
+                        <div class="col-9">
+                            <h6 id="row_data1"><span id="data_span"><?=$row2['member3']?></span></h6>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
-    <br>
-    <div class="card-header">
-        <h5>Evaluator Remarks</h5>
-    </div>
-    <div class="container grp_details">
-    <div class="row">
-            <div class="col-3">
-                <h6 id="row_data1">No Remarks Yet.</h6>
+
+    <div class="card">
+        <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+            <div class="card-header" id="headingThree">
+                <center>
+                    <h5>Abstract :- </h5>
+                </center>
             </div>
-        </div> 
+        </button>
+        <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+            <div class="card-body">
+            <div class="row">
+                        <h6 id="row_data1"><span id="data_span"><?=$row['abstract']?></span></h6>
+                    </div>
+                </div>
+            </div>
+
+
+    <div class="card">
+        <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+            <div class="card-header" id="headingTwo">
+                <center>
+                    <h5>Project Details :-</h5>
+                </center>
+            </div>
+        </button>
+        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+            <div class="card-body">
+       
+                <div class="container grp_details">
+                    <div class="row">
+                        <div class="col-3">
+                            <h6 id="row_data1">Project Name : </h6>
+                        </div>
+                        <div class="col-9">
+                            <h6 id="row_data1"><span id="data_span"><?=$row['project_name']?></span></h6>
+                        </div>
+                    </div>
+        <br>
+        <hr>
+        <br>
+        
+                    <div class="row">
+                        <div class="col-3">
+                            <h6 id="row_data1">Project Presentation : </h6>
+                        </div>
+                        <div class="col-9">
+                            <a class="btn btn-primary" href="backend/view_pdf.php?address=<?=$add?>" target="_blank">View Presentation</a>
+                        </div>
+                    </div>
+        <br>
+        <hr>
+        <br>
+                    <div class="row">
+                        <div class="col-4">
+                            <h6 id="row_data1">Research Papers Referred : </h6>
+                        </div>
+                        <div class="col-2">
+                            <a class="btn btn-primary" href="backend/view_pdf.php?address=<?=$paper1?>" target="_blank">View Paper 1</a>
+                        </div>
+                        <div class="col-2">
+                            <a class="btn btn-primary" href="backend/view_pdf.php?address=<?=$paper2?>" target="_blank">View Paper 2</a>
+                        </div>
+                        <div class="col-2">
+                            <a class="btn btn-primary" href="backend/view_pdf.php?address=<?=$paper3?>" target="_blank">View Paper 3</a>
+                        </div>
+                    </div>
+                </div>
+        <br>
+            </div>
+        </div>
+
+
+    <div class="card">
+        <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+            <div class="card-header" id="headingfour">
+                <center>
+                    <h5>Evaluator Remarks :-</h5> 
+                </center>
+            </div>
+        </button>
+        <div id="collapseFour" class="collapse" aria-labelledby="headingfour" data-parent="#accordionExample">
+            <div class="card-body">
+                <div class="container grp_details">
+                    <div class="row">
+                        <div class="col-3">
+                        <h6 id="row_data1">No Remarks Yet.</h6>
+                    </div>
+                </div>    
+            </div>
+        </div>
     </div>
+</div>
+
+
+
 
     <!-- <script>$('#projects_list').dataTable();</script> -->
     <?php
